3e42460cbdbc1b16a416a0945efa7adfcded17b1
Nicolas Trubert <nicolas.trubert@yotech.pro>
2015-11-26 14:56:25 +0100

    Port to v9 add product on demand management to hide price

--- models/models.py
+++ models/models.py
@@ -27,15 +27,34 @@ from openerp.tools.float_utils import float_round, float_compare
 import logging
 _logger = logging.getLogger(__name__)
 
+class yotech_settings(osv.osv):
+    _name = "yotech.settings"
+    _description = "Yotech settings"
 
-class yotech_setting(osv.osv):
-    _order = "name"
-    _name = "yotech.setting"
-    _description = "Yotech setting"
+    def copy(self, cr, uid, id, values, context=None):
+        raise UserError(_("Cannot duplicate configuration!"), "")
+
+    def execute(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+
+        context = dict(context, active_test=False)
+        if not self.pool['res.users']._is_admin(cr, uid, [uid]):
+            raise openerp.exceptions.AccessError(_("Only administrators can change the settings"))
+
+        # force client-side reload (update user menu and current view)
+        return {}
+
+    def cancel(self, cr, uid, ids, context=None):
+        # ignore the current record, and send the action to reopen the view
+        act_window = self.pool['ir.actions.act_window']
+        action_ids = act_window.search(cr, uid, [('res_model', '=', self._name)])
+        if action_ids:
+            return act_window.read(cr, uid, action_ids[0], [], context=context)
+        return {}
 
     _columns = {
-        'name': fields.char('Yotech Setting Name', required=True, copy=False,
-            readonly=False, select=True),
+        'name': fields.char('Yotech Setting Name', copy=False, required=True, readonly=False, select=True),
         'product_out_of_stock_mgn': fields.boolean('Product Out of Stock Management?')
     }
 
--- models/product.py
+++ models/product.py
@@ -27,30 +27,37 @@ from openerp.tools.float_utils import float_round, float_compare
 import logging
 _logger = logging.getLogger(__name__)
 
-
 class product_template(osv.osv):
-    _inherit = "product.template"
+    _name = "product.template"
+    _inherit = ['product.template']
+    _description = "Product Template"
+    _order = "name"
 
     def _website_price_displayed(self, cr, uid, ids, field_name, arg, context=None):
-        """ Return true if price must be display or not on a website
-            Ex: product out of stock and Product avaible on demand
+        """ Return true if price must be display on a website or not
+            Ex: product_out_of_stock and product_on_demand
         """
         res = {}
-        yotech_setting = self.pool['yotech.setting'].browse(cr, uid, ids, context=context)[0]
+        yotech_settings = self.pool['yotech.settings'].browse(cr, uid, [1], context=context)[0]
+        _logger.info("yotech_settings.product_out_of_stock_mgn => " + str(yotech_settings.product_out_of_stock_mgn))
 
+        _logger.info('in _website_price_displayed ...')
         for product_tmpl in self.browse(cr, uid, ids):
             res[product_tmpl.id] = True
-            if yotech_setting.product_out_of_stock_mgn:
-                if product_tmpl['qty_available'] <= 0:
-                    res[product_tmpl.id] = False
-            if product_tmpl['product_on_demand_only']:
+            if yotech_settings.product_out_of_stock_mgn:
+                _logger.info("product_tmpl.id => " + str(product_tmpl.id))
+                _logger.info("product_tmpl['type'] => " + product_tmpl['type'])
+                if product_tmpl['type'] == "product":
+                    if product_tmpl['qty_available'] <= 0:
+                        res[product_tmpl.id] = False
+            if product_tmpl['product_on_demand']:
                 res[product_tmpl.id] = False
         return res
 
     _columns = {
-        'website_price_displayed': fields.function(_website_price_displayed, string="Is price displayed", type="boolean"),
-        'product_on_demand_only': fields.boolean('Product on demand only?'),
+        'website_price_displayed': fields.function(_website_price_displayed, type="boolean", string="Is the price displayed?"),
+        'product_on_demand': fields.boolean('Product on demand?'),
         'message_type_ids': fields.many2many('message.type', 'message_type_rel', 'child_id', 'parent_id', 'Message list'),
-    }    
-    
+    }
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
