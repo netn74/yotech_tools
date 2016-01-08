d319d9752f56ddf97350d6784016ac814187f075
root <root@jonglerie-vps.yotech.ovh>
2016-01-08 09:24:00 +0100

    add loop to accept more than one attribute lines

--- models/product.py
+++ models/product.py
@@ -19,7 +19,8 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp.osv import osv, fields, expression
+from openerp.tools.translate import _
 
 import openerp.addons.decimal_precision as dp
 from openerp.tools.float_utils import float_round, float_compare
@@ -47,80 +48,88 @@ class product_product(osv.osv):
         product_new_template_info = product_info.product_new_tmpl_id
         _logger.info('product_new_template_info =) '+ str(product_new_template_info))
 
-        if len(product_old_template_info.attribute_line_ids) <> 1:
-            _logger.error('Old Template : number of Product_attribute_line must be egal to 1 not null not more than 1 number found = %s' % len(product_old_template_info.attribute_line_ids))
-            return {}
-
-        if len(product_new_template_info.attribute_line_ids) <> 1:
-            _logger.error('New Template : number of Product_attribute_line must be egal to 1 not null not more than 1 number found = %s' % len(product_new_template_info.attribute_line_ids))
+        if len(product_old_template_info.attribute_line_ids) <> len(product_new_template_info.attribute_line_ids):
+            error_Msg = 'Old Template and New Template must have the same attribute_line number  Old => %s <> New => %s' % (len(product_old_template_info.attribute_line_ids),len(product_new_template_info.attribute_line_ids))
+            raise osv.except_osv(_("Error!"), _(error_Msg))
+            _logger.error(error_Msg)
             return {}
 
         if product_old_template_info == product_new_template_info:
-            _logger.error('Old and New template are already egal')
+            error_Msg = 'Old and New template are already the same'
+            raise osv.except_osv(_("Error!"), _(error_Msg))
+            _logger.error(error_Msg)
             return {}
 
         _logger.info('product_info.ean13 =) '+ str(product_info.ean13))
         _logger.info('product_info.product_new_tmpl_id.id =) '+ str(product_info.product_new_tmpl_id.id))
 
-        attribute_line_found = False
         _logger.info('product_old_template_info.attribute_line_ids[0] =) '+ str(product_old_template_info.attribute_line_ids[0]))
         _logger.info('product_new_template_info.attribute_line_ids[0] =) '+ str(product_new_template_info.attribute_line_ids[0]))
 
-        for attribute_line_id in product_new_template_info.attribute_line_ids:
-            _logger.info('attribute_line_id =) '+ str(attribute_line_id))
-            if (product_old_template_info.attribute_line_ids[0].attribute_id.name == attribute_line_id.attribute_id.name) :
-                new_attribute_line_id = attribute_line_id
-                _logger.info('new_attribute_line_id =) '+ str(new_attribute_line_id))
-                attribute_line_found = True
-        # If attribute line found in a new template
-        if attribute_line_found:
-            old_product_tmpl_id = product_info.product_tmpl_id
-            new_product_tmpl_id = product_info.product_new_tmpl_id
+        for tmplnew_attribute_line_id in product_new_template_info.attribute_line_ids:
+            _logger.info('tmplnew_attribute_line_id =) '+ str(tmplnew_attribute_line_id))
+            for tmplold_attribute_line_id in product_old_template_info.attribute_line_ids:
+                _logger.info('tmplold_attribute_line_id =) '+ str(tmplold_attribute_line_id))
+                if (tmplold_attribute_line_id.attribute_id.name == tmplnew_attribute_line_id.attribute_id.name) :
+                    _logger.info('tmplold_attribute_line_id.attribute_id.name =) '+ str(tmplold_attribute_line_id.attribute_id.name))
+                    new_attribute_line_id = tmplnew_attribute_line_id
+                    _logger.info('new_attribute_line_id =) '+ str(new_attribute_line_id))
+                    # If attribute line found in a new template
+                    old_product_tmpl_id = product_info.product_tmpl_id
+                    new_product_tmpl_id = product_info.product_new_tmpl_id
 
-            product_attribute_line_new_info = new_attribute_line_id
-            product_attribute_line_old_info = product_old_template_info.attribute_line_ids[0]
+                    product_attribute_line_new_info = new_attribute_line_id
+                    product_attribute_line_old_info = tmplold_attribute_line_id
 
-            product_attribute_value_old_info = product_info.attribute_value_ids
-            _logger.info('product_attribute_value_old_info =) '+ str(product_attribute_value_old_info))
+                    product_attribute_value_old_info = product_info.attribute_value_ids
+                    _logger.info('product_attribute_value_old_info =) '+ str(product_attribute_value_old_info))
 
-            _logger.info('----------------- STEP1 -----------------')
-            _logger.info('Merge attribut value id to the new template')
+                    _logger.info('----------------- STEP1 -----------------')
+                    _logger.info('Merge attribut value id to the new template')
+                    #if value attribut is not yet in new tpl
+                    value_id_found = False
+                    value_id_search = tmplold_attribute_line_id.value_ids[0]
+                    _logger.info('value_id_search =) '+ str(value_id_search))
+                    for value_id in tmplnew_attribute_line_id.value_ids:
+                        _logger.info('current value_id =) '+ str(value_id))
+                        if value_id == value_id_search:
+                            value_id_found = True
+                    if not value_id_found:
+                        attribute_line_info = {
+                            'product_tmpl_id' : new_product_tmpl_id.id
+                        }
 
-            attribute_line_info = {
-                'product_tmpl_id' : new_product_tmpl_id.id
-            }
+                        _logger.info('attribute_line_info =) '+ str(attribute_line_info))
 
-            _logger.info('attribute_line_info =) '+ str(attribute_line_info))
-            product_attribute_line_obj.write(cr, uid, product_attribute_line_old_info.id, attribute_line_info, context=context)
-            SQL_UPDATE="UPDATE product_attribute_line_product_attribute_value_rel SET line_id='%s' WHERE line_id='%s'" % (product_attribute_line_new_info.id,product_attribute_line_old_info.id)
-            _logger.info(SQL_UPDATE)
-            cr.execute(SQL_UPDATE)
-            SQL_DELETE="DELETE FROM product_attribute_line WHERE id='%s' AND product_tmpl_id='%s'" % (product_attribute_line_old_info.id,new_product_tmpl_id.id)
-            _logger.info(SQL_DELETE)
-            cr.execute(SQL_DELETE)
+                        product_attribute_line_obj.write(cr, uid, product_attribute_line_old_info.id, attribute_line_info, context=context)
+                        SQL_UPDATE="UPDATE product_attribute_line_product_attribute_value_rel SET line_id='%s' WHERE line_id='%s'" % (product_attribute_line_new_info.id,product_attribute_line_old_info.id)
+                        _logger.info(SQL_UPDATE)
+                        cr.execute(SQL_UPDATE)
+                    SQL_DELETE="DELETE FROM product_attribute_line WHERE id='%s' AND product_tmpl_id='%s'" % (product_attribute_line_old_info.id,new_product_tmpl_id.id)
+                    _logger.info(SQL_DELETE)
+                    cr.execute(SQL_DELETE)
 
-            _logger.info('--------------- END STEP1 ---------------')
+                    _logger.info('--------------- END STEP1 ---------------')
 
-            _logger.info('----------------- STEP2 -----------------')
-            _logger.info('Change template id of current product')
-            product_update_info = {
-                'product_tmpl_id': product_info.product_new_tmpl_id.id,
-                'product_new_tmpl_id' : False
-            }
+        _logger.info('----------------- STEP2 -----------------')
+        _logger.info('Change template id of current product')
+        product_update_info = {
+            'product_tmpl_id': product_info.product_new_tmpl_id.id,
+            'product_new_tmpl_id' : False
+        }
 
-            _logger.info('product_update_info =) '+ str(product_update_info))
-            self.write(cr, uid, product_info.id, product_update_info, context=context)
-            _logger.info('--------------- END STEP2 ---------------')
+        _logger.info('product_update_info =) '+ str(product_update_info))
+        self.write(cr, uid, product_info.id, product_update_info, context=context)
+        _logger.info('--------------- END STEP2 ---------------')
 
-            # Disable Old Template with unique variant
-            product_template_old_update_info = {
-                'active': False
-            }
+        # Disable Old Template with unique variant
+        product_template_old_update_info = {
+            'active': False
+        }
 
-            _logger.info('product_template_old_update_info =) '+ str(product_template_old_update_info))
-            product_template_obj.write(cr, uid, old_product_tmpl_id.id, product_template_old_update_info, context=context)
-        else:
-            _logger.info('Product_attribute not found in new template check there is same attribute name in new and old template')
+        _logger.info('product_template_old_update_info =) '+ str(product_template_old_update_info))
+        product_template_obj.write(cr, uid, old_product_tmpl_id.id, product_template_old_update_info, context=context)
+
         _logger.info('button_change_template  }')
         return {}
 
@@ -137,13 +146,13 @@ class product_template(osv.osv):
     _columns = {
         'message_type_ids': fields.many2many('message.type', 'message_type_rel', 'child_id', 'parent_id', 'Message list'),
     }
-    
+
 class product_attribute_value(osv.Model):
     _inherit = "product.attribute.value"
     _columns = {
         'jsdata': fields.char("JS data", help="Here you can set a specific data to display icon on the website if the attibute type is 'Color' according to methode already implemented. exmple : ['rect2','red','blue']"),
     }
-    
+
 # Structure reference :
 # class product_attribute(osv.osv):
 #     _name = "product.attribute"
